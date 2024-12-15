d645287d8b2b4f657d0d6330e60f7e4d2bc036fc
Lfkopp <lfkopp@gmail.com>
2024-12-15 14:59:35 -0300

    coletivo

--- app.py
+++ app.py
@@ -17,10 +17,5 @@ def referencia():
     return render_template('referencia.html')
 
 
-@app.route('/<antigo>')
-def antigo():
-    return render_template('index.html')
-
-
 if __name__ == "__main__":
     app.run(host=getenv("HOST", "0.0.0.0"), port=getenv("PORT", 5000), debug=str(getenv("DEBUG", 0))==1)
